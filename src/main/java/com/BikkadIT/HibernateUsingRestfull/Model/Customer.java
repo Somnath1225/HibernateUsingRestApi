797ec77cd3f1ebb77dd7a733ff0ce4dc0fc6bab7
unknown <you@example.com>
2022-08-11 06:17:39 -0700

    ok

--- src/main/java/com/BikkadIT/HibernateUsingRestfull/Model/Customer.java
+++ src/main/java/com/BikkadIT/HibernateUsingRestfull/Model/Customer.java
@@ -1,5 +1,30 @@
 package com.BikkadIT.HibernateUsingRestfull.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
+	@Id
+	private Integer customerId;
+	private String customerName;
+	public Integer getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	@Override
+	public String toString() {
+		return "Customer [customerId=" + customerId + ", customerName=" + customerName + "]";
+	}
+	
+	
 
 }
