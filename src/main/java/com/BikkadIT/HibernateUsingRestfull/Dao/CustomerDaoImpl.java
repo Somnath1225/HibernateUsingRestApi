5187c38cd920f0c055b88bfae4db08d631b27416
Somnath1225 <somnath.dinde98@gmail.com>
2022-08-11 19:09:09 +0530

    update DaoImpl

--- src/main/java/com/BikkadIT/HibernateUsingRestfull/Dao/CustomerDaoImpl.java
+++ src/main/java/com/BikkadIT/HibernateUsingRestfull/Dao/CustomerDaoImpl.java
@@ -1,9 +1,11 @@
 package com.BikkadIT.HibernateUsingRestfull.Dao;
 
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class CustomerDaoImpl {
 
+	@Autowired
 	private SessionFactory sf;
 	
 	
