fb0d89c91981e217307579bbff8013db3aabb5f7
GNshaikh <shaikhgulab111@gmail.com>
2022-08-11 18:58:02 +0530

    entity ok

--- src/main/java/com/BikkadIT/HibernateUsingRestfull/Model/Customer.java
+++ src/main/java/com/BikkadIT/HibernateUsingRestfull/Model/Customer.java
@@ -1,5 +1,49 @@
 package com.BikkadIT.HibernateUsingRestfull.Model;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy =GenerationType.AUTO)
+	private int cId;
+	private String cName;
+	private String cAddress;
+	private String cEmail;
+	
+	public int getcId() {
+		return cId;
+	}
+	public void setcId(int cId) {
+		this.cId = cId;
+	}
+	public String getcName() {
+		return cName;
+	}
+	public void setcName(String cName) {
+		this.cName = cName;
+	}
+	public String getcAddress() {
+		return cAddress;
+	}
+	public void setcAddress(String cAddress) {
+		this.cAddress = cAddress;
+	}
+	public String getcEmail() {
+		return cEmail;
+	}
+	public void setcEmail(String cEmail) {
+		this.cEmail = cEmail;
+	}
+	@Override
+	public String toString() {
+		return "Customer [cId=" + cId + ", cName=" + cName + ", cAddress=" + cAddress + ", cEmail=" + cEmail + "]";
+	}
+	
 
 }
